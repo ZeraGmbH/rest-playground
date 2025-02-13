cc0b51bf840903b050c86488c1ab707746c115bf
mdoerr2s <m.doerr@zera.de>
2025-02-13 17:28:14 +0100

    veinentry: add simple storage for DFTModule
    Signed-off-by: mdoerr2s <m.doerr@zera.de>

--- src/veinentrysingleton.cpp
+++ src/veinentrysingleton.cpp
@@ -1,4 +1,5 @@
 #include "veinentrysingleton.h"
+#include "task_client_entity_subscribe.h"
 #include <tcpnetworkfactory.h>
 #include <QAbstractEventDispatcher>
 #include <QCoreApplication>
@@ -18,14 +19,29 @@ TaskSimpleVeinSetterPtr VeinEntrySingleton::setToVein(int entityId, QString comp
     return task;
 }
 
+VeinStorage::AbstractDatabase *VeinEntrySingleton::getStorageDb()
+{
+    return m_storage.getDb();
+}
+
 VeinEntrySingleton::VeinEntrySingleton() :
     m_tcpSystem(VeinTcp::TcpNetworkFactory::create()),
     m_cmdEventHandlerSystem(VfCmdEventHandlerSystem::create())
 {
-    m_eventHandler.addSubsystem(m_cmdEventHandlerSystem.get());
-    m_netSystem.setOperationMode(VeinNet::NetworkSystem::VNOM_PASS_THROUGH);
     m_eventHandler.addSubsystem(&m_netSystem);
     m_eventHandler.addSubsystem(&m_tcpSystem);
+    m_eventHandler.addSubsystem(m_cmdEventHandlerSystem.get());
+    m_eventHandler.addSubsystem(&m_storage);
+    m_netSystem.setOperationMode(VeinNet::NetworkSystem::VNOM_PASS_THROUGH);
+
     m_tcpSystem.connectToServer("127.0.0.1", 12000);
+
+    connect(&m_tcpSystem, &VeinNet::TcpSystem::sigConnnectionEstablished, this, [&](){
+        m_dummyComponentList = std::make_unique<QStringList>();
+
+        m_subscriberTask = TaskClientEntitySubscribe::create(1050, m_cmdEventHandlerSystem, m_dummyComponentList, 2000, [](){qWarning("foo");});
+        m_subscriberTask->start();
+    });
+
 }
 
--- src/veinentrysingleton.h
+++ src/veinentrysingleton.h
@@ -5,6 +5,7 @@
 #include <ve_eventhandler.h>
 #include <vn_networksystem.h>
 #include <vn_tcpsystem.h>
+#include <vs_clientstorageeventsystem.h>
 #include <vf_cmd_event_handler_system.h>
 #include <tasksimpleveingetter.h>
 #include <tasksimpleveinsetter.h>
@@ -24,13 +25,18 @@ public:
     TaskSimpleVeinGetterPtr getFromVein(int entityId, QString componentName);
     TaskSimpleVeinSetterPtr setToVein(int entityId, QString componentName, QVariant value);
 
+    VeinStorage::AbstractDatabase* getStorageDb();
+
 private:
     VeinEntrySingleton();
 
     VeinEvent::EventHandler m_eventHandler;
     VeinNet::NetworkSystem m_netSystem;
     VeinNet::TcpSystem m_tcpSystem;
+    VeinStorage::ClientStorageEventSystem m_storage;
     VfCmdEventHandlerSystemPtr m_cmdEventHandlerSystem;
+    std::shared_ptr<QStringList> m_dummyComponentList;
+    TaskTemplatePtr m_subscriberTask;
 
 public:
     VeinEntrySingleton(VeinEntrySingleton const&)   = delete;
